76a3230541f115929609929a84f8f61ac1b5320e
mairindeith <mairin.deith@gmail.com>
2020-07-23 13:08:28 -0700

    Cleaning up other unnecessary files

--- testScriptWindows.R
+++
@@ -1,8 +0,0 @@
-setwd("P:/Shiny_vanPoorten/PVAInvasR/R")
-
-library(tidyverse)
-library(devtools)
-load_all()
-
-inputs <- load_pva_parameters("../../ShinyDropbox/TestInputs/DouglasYP_population_parameters_v0.csv")
-
